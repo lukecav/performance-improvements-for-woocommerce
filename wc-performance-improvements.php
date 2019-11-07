c4c7b5c82812acd7120c6e9b91e00f18e1d654d2
Luke Cavanagh <lukecav@users.noreply.github.com>
2019-11-07 11:37:44 -0700

    Update wc-performance-improvements.php
    Bump WooCommerce version and add code for Remove the WooCommerce Admin Install Nag.

--- wc-performance-improvements.php
+++ wc-performance-improvements.php
@@ -3,14 +3,14 @@
  * Plugin Name:          Performance Improvements for WooCommerce
  * Plugin URI:           https://github.com/lukecav/performance-improvements-for-woocommerce
  * Description:          Performance tweaks related to orders on the front-end and the back-end of a store. Will also disable dashboard widgets for reviews and status in WooCommerce. Also includes specific tweaks for products in the back-end of the store.
- * Version:              1.0.5
+ * Version:              1.0.6
  * Author:               Luke Cavanagh
  * Author URI:           https://github.com/lukecav
  * License:              GPL2
  * License URI:          https://www.gnu.org/licenses/gpl-2.0.html
  *
- * WC requires at least: 3.0.0
- * WC tested up to:      3.6.2
+ * WC requires at least: 3.5.8
+ * WC tested up to:      3.8.0
  *
  * @package WooCommerce_Performance_Improvements
  * @author  Luke Cavanagh
@@ -77,5 +77,14 @@ add_filter( 'woocommerce_allow_marketplace_suggestions', '__return_false' );
 // Remove connect your store to WooCommerce.com admin notice
 add_filter( 'woocommerce_helper_suppress_admin_notices', '__return_true' );
 
-// Deregister block style from WooCommerce
+// Remove the WooCommerce Admin Install Nag
 wp_deregister_style( 'wc-block-style' );
+
+// Deregister block style from WooCommerce
+add_filter( 'woocommerce_show_admin_notice', 'wc_disable_wc_admin_install_notice', 10, 2 );
+function wc_disable_wc_admin_install_notice( $notice_enabled, $notice ) {
+	if ( 'wc_admin' === $notice ) {
+		return false;
+	}
+	return $notice_enabled;
+}
