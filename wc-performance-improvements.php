3d825768c26ca361b87dcbe677526c5e22e9c4e2
Luke Cavanagh <lukecav@users.noreply.github.com>
2022-05-11 11:43:04 -0600

    Update wc-performance-improvements.php
    Bump version.

--- wc-performance-improvements.php
+++ wc-performance-improvements.php
@@ -3,7 +3,7 @@
  * Plugin Name:          Performance Improvements for WooCommerce
  * Plugin URI:           https://github.com/lukecav/performance-improvements-for-woocommerce
  * Description:          Performance tweaks for WooCommerce.
- * Version:              1.1.5
+ * Version:              1.1.6
  * Author:               Luke Cavanagh
  * Author URI:           https://github.com/lukecav
  * Requires at least:    5.6
