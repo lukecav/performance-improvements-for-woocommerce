ffbf620425391d08b31bb36b9d5cb196cb5ecc27
Luke Cavanagh <lukecav@users.noreply.github.com>
2022-06-14 10:38:11 -0600

    Update wc-performance-improvements.php
    Bump WooCommerce tested version.
    https://developer.woocommerce.com/2022/06/14/woocommerce-6-6-released/

--- wc-performance-improvements.php
+++ wc-performance-improvements.php
@@ -12,7 +12,7 @@
  * License URI:          https://www.gnu.org/licenses/gpl-2.0.html
  *
  * WC requires at least: 5.0
- * WC tested up to:      6.5
+ * WC tested up to:      6.6
  *
  * @package WooCommerce_Performance_Improvements
  * @author  Luke Cavanagh
